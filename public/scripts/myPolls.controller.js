16765c7f64568bc3ca9d2cf08bcef75a847d2c30
qazxswedc <sandeep.carmel@gmail.com>
2017-09-05 17:37:25 +0000

    removed some log

--- public/scripts/myPolls.controller.js
+++ public/scripts/myPolls.controller.js
@@ -6,9 +6,7 @@ $(document).ready(function(){
     });
 });
 function loadPolls(data) {
-    //console.log(data);
     for(var i in data){
-        console.log(data[i]);
         var poll = data[i];
             //Have to add route /polls/pollId
         var pollHTML = "<li><a href='/polls/"+poll._id+"'>"+poll.question+"</a><button><a href='/results/"+poll._id+"'>View Results</a></button></li>";
